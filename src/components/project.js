30fd2d94efafad40c852b96128f087e5f8f23747
zied <ziedgmar242@gmail.com>
2020-03-31 09:49:44 -0500

    router navigation

---
+++ src/components/project.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+     
+        
+class Project extends Component{
+    render(){
+
+        return(
+
+          <div><h1>Project  page</h1></div>
+
+        );
+    }
+}
+export default Project;
